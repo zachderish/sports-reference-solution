dd44c764a899263512c4e15cf48325547b747b30
Zach <zderish@gmail.com>
2024-01-07 14:54:41 -0500

    Add styling

--- index.js
+++ index.js
@@ -1,149 +1,5 @@
 // JSON object
-const object = {
-
-    'BRO': {
-    
-    'BSN': { 'W': 10, 'L': 12 },
-    
-    'CHC': { 'W': 15, 'L': 7 },
-    
-    'CIN': { 'W': 15, 'L': 7 },
-    
-    'NYG': { 'W': 14, 'L': 8 },
-    
-    'PHI': { 'W': 14, 'L': 8 },
-    
-    'PIT': { 'W': 15, 'L': 7 },
-    
-    'STL': { 'W': 11, 'L': 11 }
-    
-    },
-    
-    'BSN': {
-    
-    'BRO': { 'W': 12, 'L': 10 },
-    
-    'CHC': { 'W': 13, 'L': 9 },
-    
-    'CIN': { 'W': 13, 'L': 9 },
-    
-    'NYG': { 'W': 13, 'L': 9 },
-    
-    'PHI': { 'W': 14, 'L': 8 },
-    
-    'PIT': { 'W': 12, 'L': 10 },
-    
-    'STL': { 'W': 9, 'L': 13 }
-    
-    },
-    
-    'CHC': {
-    
-    'BRO': { 'W': 7, 'L': 15 },
-    
-    'BSN': { 'W': 9, 'L': 13 },
-    
-    'CIN': { 'W': 12, 'L': 10 },
-    
-    'NYG': { 'W': 7, 'L': 15 },
-    
-    'PHI': { 'W': 16, 'L': 6 },
-    
-    'PIT': { 'W': 8, 'L': 14 },
-    
-    'STL': { 'W': 10, 'L': 12 }
-    
-    },
-    
-    'CIN': {
-    
-    'BRO': { 'W': 7, 'L': 15 },
-    
-    'BSN': { 'W': 9, 'L': 13 },
-    
-    'CHC': { 'W': 10, 'L': 12 },
-    
-    'NYG': { 'W': 13, 'L': 9 },
-    
-    'PHI': { 'W': 13, 'L': 9 },
-    
-    'PIT': { 'W': 13, 'L': 9 },
-    
-    'STL': { 'W': 8, 'L': 14 }
-    
-    },
-    
-    'NYG': {
-    
-    'BRO': { 'W': 8, 'L': 14 },
-    
-    'BSN': { 'W': 9, 'L': 13 },
-    
-    'CHC': { 'W': 15, 'L': 7 },
-    
-    'CIN': { 'W': 9, 'L': 13 },
-    
-    'PHI': { 'W': 12, 'L': 10 },
-    
-    'PIT': { 'W': 15, 'L': 7 },
-    
-    'STL': { 'W': 13, 'L': 9 }
-    
-    },
-    
-    'PHI': {
-    
-    'BRO': { 'W': 8, 'L': 14 },
-    
-    'BSN': { 'W': 8, 'L': 14 },
-    
-    'CHC': { 'W': 6, 'L': 16 },
-    
-    'CIN': { 'W': 9, 'L': 13 },
-    
-    'NYG': { 'W': 10, 'L': 12 },
-    
-    'PIT': { 'W': 13, 'L': 9 },
-    
-    'STL': { 'W': 8, 'L': 14 }
-    
-    },
-    
-    'PIT': {
-    
-    'BRO': { 'W': 7, 'L': 15 },
-    
-    'BSN': { 'W': 10, 'L': 12 },
-    
-    'CHC': { 'W': 14, 'L': 8 },
-    
-    'CIN': { 'W': 9, 'L': 13 },
-    
-    'NYG': { 'W': 7, 'L': 15 },
-    
-    'PHI': { 'W': 9, 'L': 13 },
-    
-    'STL': { 'W': 6, 'L': 16 }
-    
-    },
-    
-    'STL': {
-    
-    'BRO': { 'W': 11, 'L': 11 },
-    
-    'BSN': { 'W': 13, 'L': 9 },
-    
-    'CHC': { 'W': 12, 'L': 10 },
-    
-    'CIN': { 'W': 14, 'L': 8 },
-    
-    'NYG': { 'W': 9, 'L': 13 },
-    
-    'PHI': { 'W': 14, 'L': 8 },
-    
-    'PIT': { 'W': 16, 'L': 6 }
-    
-    }};
+const object = "Some JSON";
 
 // Get length from object
 const keys = Object.keys(object)
@@ -159,6 +15,7 @@ const headerRow = header.insertRow(START_INDEX);
 for (let j = START_INDEX; j < length + 1; j++) {
 
     let cell = headerRow.insertCell(j);
+    cell.classList.add("header-data");
 
     if (j === START_INDEX) {
         cell.innerHTML = "Tm";
@@ -166,6 +23,10 @@ for (let j = START_INDEX; j < length + 1; j++) {
 
     else {
         cell.innerHTML = keys[j - 1];
+
+        if (j === length) {
+            cell.classList.add("last-column");
+        }
     }
 }
 
@@ -177,17 +38,34 @@ for (let i = START_INDEX; i < length + 1; i++) {
     let row = body.insertRow(i);
 
     let cell = row.insertCell(START_INDEX);
+    cell.classList.add("left");
     // If body row is the footer or not
+    if (i === length - 1) {
+        cell.classList.add("second-last-row");
+    }
     if (i === length) {
         cell.innerHTML = "Tm";
+        cell.classList.add("last-row");
     }
     else {
         cell.innerHTML = keys[i];
+
     }
 
     for (let j = START_INDEX; j < length; j++) {
 
         let cell = row.insertCell(j + 1);
+
+        if (j === length - 1) {
+            cell.classList.add("last-column");
+        }
+        if (i === length - 1) {
+            cell.classList.add("second-last-row");
+        }
+        if (i === length) {
+            cell.classList.add("last-row");
+        }
+
 
         // Get team i
         let team = keys[i];
@@ -202,10 +80,12 @@ for (let i = START_INDEX; i < length + 1; i++) {
             if (i != j) {
                 let versusTeam = keys[j];
                 cell.innerHTML = object[team][versusTeam].W;
+                cell.classList.add("wins");
             }
             
             else {
                 cell.innerHTML = "--";
+                cell.classList.add("wins");
             }
         }
     }
